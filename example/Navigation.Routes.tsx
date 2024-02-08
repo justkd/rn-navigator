e40d9ff68ce6e273604b9c900db922191ab39059
justKD <cady.holmes.dev@gmail.com>
2024-02-08 16:26:00 +0000

    feat: Fix import and add getTypedRoutes function

--- example/Navigation.Routes.tsx
+++ example/Navigation.Routes.tsx
@@ -1,17 +1,9 @@
-import type { ComponentType } from 'react'
+import { getTypedRoutes } from './getTypedRoutes'
 import { Home } from './routes/Home'
 import { One } from './routes/One'
 import { OneA } from './routes/One.A'
 import { Two } from './routes/Two'
 import { TwoA } from './routes/Two.A'
-
-const getTypedRoutes = <T extends string>(
-  routes: Record<T, ComponentType>,
-) => {
-  type RouteKey = keyof typeof routes
-  const Routes: Record<RouteKey, ComponentType> = routes
-  return Routes
-}
 
 /* ******************** */
 
@@ -25,6 +17,6 @@ const routes = {
   '/Two/A': TwoA,
 }
 
-export const Routes = getTypedRoutes<keyof typeof routes>(routes)
 export type RouteKey = keyof typeof routes
 export type RoutesKeys = Record<RouteKey, RouteKey>
+export const Routes = getTypedRoutes<RouteKey>(routes)
