4171a0a4472c6f7af446376f1aab04138d3d0e0c
justKD <cady.holmes.dev@gmail.com>
2024-02-27 20:33:28 +0000

    fix: Fix translation animation bug with base duration not being updated correctly

--- src/lib/animations/translateLTR.ts
+++ src/lib/animations/translateLTR.ts
@@ -5,7 +5,9 @@ import { type NavigationAnimations } from '../Navigation.types'
 /**
  * Animation collection for a given navigation transition style.
  * All of these should be in hook format that returns an object
- * `{ anims: NavigationAnimations }`
+ * `{ anims: NavigationAnimations, baseDur: number }`. `baseDur`
+ * is the duration of a single transition animation (half of a
+ * transition).
  * @note
  * Adding other animation types might need some reworking of how
  * anim refs are being passed and handled. I can imagine a transition
@@ -20,102 +22,81 @@ export const useTranslateLTR = (
   const animate = (to: number, easing: EasingFunction) => ({
     toValue: to,
     duration: baseDur,
-    easing,
+    easing: Easing.inOut(easing),
     useNativeDriver: true,
   })
 
-  const translateLTR: NavigationAnimations = useMemo(
-    () => ({
+  const translateLTR: NavigationAnimations = useMemo(() => {
+    const T = animTranslate.current
+    const O = animOpacity.current
+    const { exp, ease, bounce } = Easing
+    const { timing, parallel, sequence } = Animated
+    return {
       /* =^..^=  ✿  =^..^=  */
       backIn: () => {
         animTranslate.current.setValue(2)
-        return Animated.parallel([
-          Animated.timing(
-            animTranslate.current,
-            animate(1, Easing.in(Easing.exp)),
-          ),
-          Animated.timing(
-            animOpacity.current,
-            animate(1, Easing.inOut(Easing.ease)),
-          ),
+        return parallel([
+          timing(T, animate(1, exp)),
+          timing(O, animate(1, ease)),
         ])
       },
       /* =^..^=  ✿  =^..^=  */
-      backOut: Animated.parallel([
-        Animated.timing(
-          animTranslate.current,
-          animate(-1, Easing.inOut(Easing.exp)),
-        ),
-        Animated.timing(
-          animOpacity.current,
-          animate(0, Easing.inOut(Easing.ease)),
-        ),
+      backOut: parallel([
+        timing(T, animate(-1, exp)),
+        timing(O, animate(0, ease)),
       ]),
       /* =^..^=  ✿  =^..^=  */
-      in: Animated.parallel([
-        Animated.timing(
-          animTranslate.current,
-          animate(1, Easing.inOut(Easing.exp)),
-        ),
-        Animated.timing(
-          animOpacity.current,
-          animate(1, Easing.inOut(Easing.ease)),
-        ),
+      in: parallel([
+        timing(T, animate(1, exp)),
+        timing(O, animate(1, ease)),
       ]),
       /* =^..^=  ✿  =^..^=  */
-      out: Animated.parallel([
-        Animated.timing(
-          animTranslate.current,
-          animate(2, Easing.inOut(Easing.exp)),
-        ),
-        Animated.timing(
-          animOpacity.current,
-          animate(0, Easing.inOut(Easing.ease)),
-        ),
+      out: parallel([
+        timing(T, animate(2, exp)),
+        timing(O, animate(0, ease)),
       ]),
       /* =^..^=  ✿  =^..^=  */
       error: () => {
-        const stage = (to: number) =>
-          Animated.timing(animTranslate.current, {
-            toValue: to,
-            duration: baseDur / 10,
-            easing: Easing.bounce,
-            useNativeDriver: true,
-          })
-        animTranslate.current.setValue(1)
         const base = 1
         const delta = (n: number) => n * (base + 0.04)
-        const lerp = (n: number) =>
+        const scale = (n: number) =>
           n + 1 < 0
             ? base - Math.log10(Math.abs(n - 1))
             : base + Math.log10(n + 1)
-        return Animated.sequence([
-          stage(lerp(delta(0.05))),
-          stage(lerp(delta(-0.05))),
-          stage(lerp(delta(0.025))),
-          stage(lerp(delta(-0.025))),
-          stage(lerp(delta(0.0125))),
-          stage(lerp(delta(-0.0125))),
-          stage(lerp(delta(0.001))),
-          stage(lerp(delta(-0.001))),
+        const stage = (to: number) =>
+          timing(T, {
+            toValue: to,
+            duration: baseDur / 9,
+            easing: bounce,
+            useNativeDriver: true,
+          })
+        T.setValue(1)
+        return sequence([
+          stage(scale(delta(0.05))),
+          stage(scale(delta(-0.05))),
+          stage(scale(delta(0.025))),
+          stage(scale(delta(-0.025))),
+          stage(scale(delta(0.0125))),
+          stage(scale(delta(-0.0125))),
+          stage(scale(delta(0.001))),
+          stage(scale(delta(-0.001))),
           stage(base),
         ])
       },
       /* =^..^=  ✿  =^..^=  */
       reset: (cb?: () => void) => {
-        animTranslate.current.stopAnimation(() => {
-          animTranslate.current.removeAllListeners()
-          animOpacity.current.stopAnimation(() => {
-            animOpacity.current.removeAllListeners()
-            animOpacity.current.setValue(0)
-            animTranslate.current.setValue(0)
+        T.stopAnimation(() => {
+          T.removeAllListeners()
+          O.stopAnimation(() => {
+            O.removeAllListeners()
+            O.setValue(0)
+            T.setValue(0)
             cb?.()
           })
         })
       },
       /* =^..^=  ✿  =^..^=  */
-    }),
-    [animOpacity, animTranslate],
-  )
+    }
+  }, [animOpacity, animTranslate])
   return { anims: translateLTR, baseDur }
 }
